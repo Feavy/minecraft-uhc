73e85f9440ddf34a4304b510c08c790bf062847b
Feavy <quentin071100@live.fr>
2022-08-18 16:23:54 +0200

    Set server.properties and naturalGeneration false

--- MinecraftUHC/src/main/java/fr/feavy/mc/Main.java
+++ MinecraftUHC/src/main/java/fr/feavy/mc/Main.java
@@ -1,6 +1,9 @@
 package fr.feavy.mc;
 
 import java.awt.Color;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 
@@ -8,6 +11,11 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         super.onEnable();
         this.getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+
+        for (World world : Bukkit.getServer().getWorlds()) {
+            world.setGameRuleValue("naturalRegeneration", "false");
+        }
+
         Discord.discord().sendEmbed(
                 new EmbedBuilder()
                         .setTitle("Le serveur est ON")
